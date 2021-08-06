42fe2fb6b12ef50f02035b6322012e6861ff185d
Jon Bernie <handuy099@gmail.com>
2021-08-06 20:23:05 +0700

    add folder zsh

---
+++ zsh/.config/zsh/aliases.zsh
@@ -0,0 +1,8 @@
+alias nv='nvim'
+alias lv='lvim'
+alias ll='colorls -lA --sd --group-directories-first'
+alias ls='colorls --group-directories-first'
+alias glst='git log -1 HEAD'
+alias live='live-server'
+alias xrn='xrandr --newmode'
+alias xra='xrandr --addmode VGA-1'
