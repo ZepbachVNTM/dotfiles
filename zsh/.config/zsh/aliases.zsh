07c4e326049099faacf9670d530914eb6176586c
DavidH <handuy099@gmail.com>
2022-11-15 19:30:46 +0700

    use zap plugin manager

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,12 +1,12 @@
 #!/bin/sh
-alias zsh-update-plugins="find "$ZDOTDIR/plugins" -type d -exec test -e '{}/.git' ';' -print0 | xargs -I {} -0 git -C {} pull -q"
+alias j="z"
+alias f="zi"
+alias g="lazygit"
+alias nvimrc='nvim ~/.config/nvim/'
 
-alias uget="aria2c -x 16 -s 16"
-
-alias rm='rm -r'
-
-if [ $EDITOR = 'lvim' ]; then
+if [ $EDITOR='lvim' ]; then
   alias nv="lvim"
+	alias lv="lvim"
 else
   alias nv="nvim"
 fi
@@ -15,14 +15,15 @@ if [ $(command -v live-server) ]; then
 	alias live="live-server"
 fi
 
-
-# Remarkable
-alias remarkable_ssh='ssh root@10.11.99.1'
-
 # Colorize grep output (good for log files)
 alias grep='grep --color=auto'
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
+
+# confirm before overwriting something
+alias cp="cp -i"
+alias mv='mv -i'
+alias rm='rm -i'
 
 # easier to read disk
 alias df='df -h'     # human-readable sizes
@@ -34,10 +35,14 @@ alias psmem='ps auxf | sort -nr -k 4 | head -5'
 # get top process eating cpu ##
 alias pscpu='ps auxf | sort -nr -k 3 | head -5'
 
+# gpg encryption
 # verify signature for isos
 alias gpg-check="gpg2 --keyserver-options auto-key-retrieve --verify"
 # receive the key of a developer
 alias gpg-retrieve="gpg2 --keyserver-options auto-key-retrieve --receive-keys"
+
+# For when keys break
+alias archlinx-fix-keys="sudo pacman-key --init && sudo pacman-key --populate archlinux && sudo pacman-key --refresh-keys"
 
 # systemd
 alias mach_list_systemctl="systemctl list-unit-files --state=enabled"
@@ -50,8 +55,10 @@ Darwin)
 	;;
 
 Linux)
-  if [ $(command -v colorls) ]; then
-   	alias ls='colorls --sd --color=auto'
+  if [ $(command -v lsd) ]; then
+    alias ls="lsd --group-directories-first"
+    alias ll="lsd -lA --group-directories-first"
+    alias la="lsd -A --group-directories-first"
   else
     alias ls='ls --color=auto'
   fi
