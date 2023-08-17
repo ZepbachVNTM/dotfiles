68c1d55a50c798406faea8028a833c498b7bc997
Akatsuki <handuy099@gmail.com>
2023-08-18 06:16:29 +0700

    update

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -1,7 +1,14 @@
 #!/bin/sh
+
+# XDG Paths
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_CACHE_HOME=$HOME/.cache
+export XDG_DATA_HOME=$HOME/.local/share
+
 HISTFILE="$XDG_DATA_HOME"/zsh/history
 HISTSIZE=1000000
 SAVEHIST=1000000
 
 # eval "`pip completion --zsh`"
 eval "$(zoxide init zsh)"
+source /usr/share/nvm/init-nvm.sh
