7116a35e9ac563987a61135aa29cad283702d334
Akatsuki <handuy099@gmail.com>
2023-08-22 13:56:42 +0700

    update

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -4,6 +4,8 @@ HISTFILE="$XDG_DATA_HOME"/zsh/history
 HISTSIZE=1000000
 SAVEHIST=1000000
 
+export PATH="$HOME/.config/emacs/bin/:$PATH"
+
 # eval "`pip completion --zsh`"
 eval "$(zoxide init zsh)"
 source /usr/share/nvm/init-nvm.sh
--- zsh/.zshenv
+++ zsh/.zshenv
@@ -9,3 +9,5 @@ export GTK_IM_MODULE='fcitx'
 export QT_IM_MODULE='fcitx'
 export SDL_IM_MODULE='fcitx'
 export XMODIFIERS='@im=fcitx'
+
+export BROWSER="firefox"
